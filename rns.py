5b2b35b7efb2af91556303e68b95e8239065a1a0
Alexey Bednyakov <cheshir.box@gmail.com>
2011-12-15 16:31:05 +0300

    Fastest (and working) comparison

--- rns.py
+++ rns.py
@@ -26,6 +26,36 @@ def mul(a):
         x *= i
     return x
 
+def comparer(A, B, s = '='):
+    if type(A) is int:
+        if A > B:
+            return '>'
+        elif A < 0:
+            return '<'
+        else:
+            return '='
+    Ai = [(a - A.vector[0]) for a in A.vector]
+    Bi = [(b - B.vector[0]) for b in B.vector]
+    if Ai == Bi:
+        if A.vector[0] > B.vector[0]:
+            return '>'
+        elif A.vector[0] < B.vector[0]:
+            return '<'
+        else:
+            return '='
+    else:
+        if A.vector[0] < B.vector[0]:
+            situation = '<'
+        elif A.vector[0] > B.vector[0]:
+            situation = '>'
+        else:
+            situation = '='
+        Am = [(A.vector[i] * negative(A.modules[0], A.modules[i])) for i in range(1, len(Ai))]
+        Bm = [(B.vector[i] * negative(B.modules[0], B.modules[i])) for i in range(1, len(Bi))]
+        return comparer(RNS(Am, A.modules[1:]),
+                        RNS(Bm, B.modules[1:]),
+                        situation)
+
 class RNS(numbers.Number):
     """This class represents numbers in residue number system (RNS).
     """
@@ -34,19 +64,19 @@ class RNS(numbers.Number):
 
     def __new__(cls, number = 0, modules = ()):
         """Constructs an RNS number.
-        With execution GCD(X, Y) possible arguments types is:
+        When executing like this: GCD(X, Y), possible arguments types are:
             * Both X and Y are integer. Then we get GCD representation
               in system with Y limit;
             * When X is integer and Y is tuple or list. Then we get X
               representation in system of Y modules;
-            * Finally, when X is list while Y is tuple. Then we generate GCD
+            * Finally, when X and Y are tuple or list. Then we generate GCD
               object with X vector and Y modules without any verification.
         """
         self = super(RNS, cls).__new__(cls)
         if type(number) in (int, long):
             if type(modules) is int:
                 self.generate_modules(modules)
-            if type(modules) in (tuple, list) and len(modules) > 0:
+            elif type(modules) in (tuple, list) and len(modules) > 0:
                 representation = [number % m for m in modules]
                 self._vector = representation
                 self._modules = modules
@@ -68,10 +98,31 @@ class RNS(numbers.Number):
     def dec(a):
         return a._dec
 
+    @property
+    def amrs(a):
+        return a._amrs
+
     @classmethod
     def to_decimal(self, vector, modules):
         """Garner algorithm:
-        Converts an RNS number to a decimail number.
+        Converts an RNS number to a decimal number.
+        """
+        c = {}
+        for i in range(2, len(modules) + 1):
+            c[i] = 1
+            for j in range(1, i):
+                u = negative(modules[j - 1], modules[i - 1])
+                c[i] = (c[i] * u) % modules[i - 1]
+        x = u = vector[0]
+        for i in range(2, len(modules) + 1):
+            print
+            u = ((vector[i - 1] - x) * c[i]) % modules[i - 1]
+            x = x + u * mul(modules[:i - 1])
+        self._decimal = x
+
+    @classmethod
+    def to_amrs(self, vector, modules):
+        """Convention to the associated mixed radix system.
         """
         c = {}
         for i in range(2, len(modules) + 1):
@@ -121,6 +172,73 @@ class RNS(numbers.Number):
         return '[ ' + ''.join('(%d mod %d) ' % (x, y)
             for (x, y) in izip(self._vector, self._modules)) + ']'
 
+    def _add(a, b):
+        """a + b"""
+        if a.modules != b.modules or len(a.vector) != len(b.vector):
+            raise TypeError("can't operate with numbers in different modular systems.")
+        return RNS([(x + y) % m for (x, y, m) in izip(a.vector, b.vector, a.modules)], a.modules)
+
+    def _sub(a, b):
+        """a - b"""
+        if a.modules != b.modules or len(a.vector) != len(b.vector):
+            raise TypeError("can't operate with numbers in different modular systems.")
+        return RNS([(x - y) % m for (x, y, m) in izip(a.vector, b.vector, a.modules)], a.modules)
+
+    def _mul(a, b):
+        """a * b"""
+        if a.modules != b.modules or len(a.vector) != len(b.vector):
+            raise TypeError("can't operate with numbers in different modular systems.")
+        return RNS([(x * y) % m for (x, y, m) in izip(a.vector, b.vector, a.modules)], a.modules)
+
+    def _div(a, b):
+        """a / b"""
+        raise NotImplemented
+
+    def __hash__(self):
+        """hash(self)"""
+        return hash((self._vector, self._modules))
+
+    def __nonzero__(a):
+        """a != 0"""
+        return (a._vector == [0 for i in range(0, len(a._vector))])
+
+    def __eq__(a, b):
+        """a == b"""
+        if isinstance(b, RNS):
+            return (a._vector == b.vector and a._modules == b.modules)
+
+    def __lt__(a, b):
+        """a < b"""
+        if isinstance(b, RNS):
+            if comparer(a, b) == '<':
+                return True
+            else:
+                return False
+
+    def __gt__(a, b):
+        """a > b"""
+        if isinstance(b, RNS):
+            if comparer(a, b) == '>':
+                return True
+            else:
+                return False
+
+    def __le__(a, b):
+        """a <= b"""
+        if isinstance(b, RNS):
+            if a == b or a < b:
+                return True
+            else:
+                return False
+
+    def __ge__(a, b):
+        """a >= b"""
+        if isinstance(b, RNS):
+            if a == b or a > b:
+                return True
+            else:
+                return False
+
     def _operator_fallbacks(monomorphic_operator, fallback_operator):
         """Generates forward and reverse operators given a purely-rational
         operator and a function from the operator module.
@@ -151,68 +269,20 @@ class RNS(numbers.Number):
         reverse.__doc__ = monomorphic_operator.__doc__
         return forward, reverse
 
-    def _add(a, b):
-        """a + b"""
-        if a.modules != b.modules or len(a.vector) != len(b.vector):
-            raise TypeError("Cannot operate with numbers in different modular systems.")
-        return RNS([(x + y) % m for (x, y, m) in izip(a.vector, b.vector, a.modules)], a.modules)
-
     __add__, __radd__ = _operator_fallbacks(_add, operator.add)
-
-    def _sub(a, b):
-        """a - b"""
-        if a.modules != b.modules or len(a.vector) != len(b.vector):
-            raise TypeError("Cannot operate with numbers in different modular systems.")
-        return RNS([(x - y) % m for (x, y, m) in izip(a.vector, b.vector, a.modules)], a.modules)
-
     __sub__, __rsub__ = _operator_fallbacks(_sub, operator.sub)
-
-    def _mul(a, b):
-        """a * b"""
-        if a.modules != b.modules or len(a.vector) != len(b.vector):
-            raise TypeError("Cannot operate with numbers in different modular systems.")
-        return RNS([(x * y) % m for (x, y, m) in izip(a.vector, b.vector, a.modules)], a.modules)
-
     __mul__, __rmul__ = _operator_fallbacks(_mul, operator.mul)
 
-    def _div(a, b):
-        """a / b"""
-
-    def __hash__(self):
-        """hash(self)"""
-        return hash((self._vector, self._modules))
-
-    def __eq__(a, b):
-        """a == b"""
-        if isinstance(b, RNS):
-            return (a._vector == b.vector and a._modules == b.modules)
-
-    def __lt__(a, b):
-        """a < b"""
-
-    def __gt__(a, b):
-        """a > b"""
-
-    def __le__(a, b):
-        """a <= b"""
-
-    def __ge__(a, b):
-        """a >= b"""
-
-    def __nonzero__(a):
-        """a != 0"""
-
-    # support for pickling, copy, and deepcopy
     def __reduce__(self):
         return (self.__class__, (str(self),))
 
     def __copy__(self):
         if type(self) == RNS:
-            return self     # I'm immutable; therefore I am my own clone
+            return self
         return self.__class__(self._vector, self._modules)
 
     def __deepcopy__(self, memo):
         if type(self) == RNS:
-            return self     # My components are also immutable
+            return self
         return self.__class__(self._vector, self._modules)
 
